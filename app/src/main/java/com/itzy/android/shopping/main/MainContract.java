964c86ce18f282d04a3e79d889def93d7095e25e
leesungwoo <tols3319@gmail.com>
2019-04-15 22:44:24 +0900

    modify class to interface

--- app/src/main/java/com/itzy/android/shopping/main/MainContract.java
+++ app/src/main/java/com/itzy/android/shopping/main/MainContract.java
@@ -3,7 +3,7 @@ package com.itzy.android.shopping.main;
 import com.itzy.android.shopping.BasePresenter;
 import com.itzy.android.shopping.BaseView;
 
-public class MainContract {
+public interface MainContract {
     interface View extends BaseView<MainContract.Presenter> {
 
     }
